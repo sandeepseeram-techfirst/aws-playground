32c0d5366c22c22bc3ce1c5ef3a5dec1f0a9fa8e
root <seerams@acm.org>
2020-05-16 12:26:16 +0000

    Sat May 16 2020 - Committed from Codeherent platform

---
+++ elasticbeanstalk.tf
@@ -0,0 +1,11 @@
+resource "aws_accessanalyzer_analyzer" {}
+
+resource "aws_elastic_beanstalk_application" {}
+
+resource "aws_s3_bucket" {}
+
+resource "aws_vpc" {}
+
+resource "aws_vpc_endpoint_subnet_association" {}
+
+resource "aws_route" {}
